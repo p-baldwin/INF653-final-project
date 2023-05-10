c290786bfc2489acc6779421e4f49e363aee796e
p-baldwin <p_baldwin@mail.fhsu.edu>
2023-05-09 22:17:31 -0500

    Troubleshooting Glitch Failing Still

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const errorHandler = require('./middleware/errorHandler');
 const mongoose = require('mongoose');
 const connectDB = require('./config/dbConn');
 
-const PORT = process.env.PORT || 3500;
+const PORT = process.env.PORT || 3000;
 
 // Connect to MongoDB
 connectDB();
